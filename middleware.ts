9e365dcc4a41824eda4385dc694095ab71bd2055
Harshalvk <harshalvkhobragade@gmail.com>
2025-07-18 16:02:55 +0530

    feat(auth): integrate better-auth for authentication and session management
    - Added API routes for authentication using better-auth.
    - Implemented auth client for session handling in the frontend.
    - Configured better-auth with Prisma adapter for PostgreSQL.
    - Created middleware to protect routes and manage session state.
    - Updated Prisma schema to include user, session, account, and verification models.
    - Added migration scripts for the new database schema.

---
+++ middleware.ts
@@ -0,0 +1,48 @@
+import { betterFetch } from "@better-fetch/fetch";
+import type { auth } from "@/lib/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+type Session = typeof auth.$Infer.Session;
+
+const authRoutes = ["/sign-in", "/sign-up"];
+const protectuedRoutes = ["/dashboard"];
+
+export async function middleware(request: NextRequest) {
+  const pathName = request.nextUrl.pathname;
+  const isAuthRoute = authRoutes.includes(pathName);
+  const isProtectedRoute = protectuedRoutes.includes(pathName);
+
+  const { data: session } = await betterFetch<Session>(
+    "/api/auth/get-session",
+    {
+      baseURL: request.nextUrl.origin,
+      headers: {
+        cookie: request.headers.get("cookie") || "", // Forward the cookies from the request
+      },
+    },
+  );
+
+  console.log("SESSION####", session);
+
+  if (!session) {
+    if (isAuthRoute) {
+      return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  if (session) {
+    if (isAuthRoute) {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+    if (isProtectedRoute) {
+      return NextResponse.next();
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard"], // Apply middleware to specific routes
+};
