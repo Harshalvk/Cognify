2b67aa6e15081756a502fc518d388939d6102fd1
Harshalvk <harshalvkhobragade@gmail.com>
2025-07-19 12:16:52 +0530

    add: user forgot & reset password validation schema

--- validator/user-auth.ts
+++ validator/user-auth.ts
@@ -10,3 +10,12 @@ export const userSignInFormSchema = z.object({
   email: z.string(),
   password: z.string().min(2).max(32),
 });
+
+export const userForgotPasswordFormSchema = z.object({
+  email: z.string(),
+});
+
+export const userResetPasswordFormSchema = z.object({
+  password: z.string(),
+  confirmPassword: z.string(),
+});
