c5ebb3904d474d4e3f41373402455e9e2de6f6c9
Harshalvk <harshalvkhobragade@gmail.com>
2025-07-20 22:37:37 +0530

    feat: add hooks in auth, runs after a user sign-in which adds userId to embeddings vectorDB - disable auto-signIn - currently this feature is only working when a user sign-up with email&password and not with social provider - need to reproduce this to work with social provider also

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,10 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma";
 import { nextCookies } from "better-auth/next-js";
 import { sendForgotPasswordMail, sendVerificationEmailAction } from "./actions";
+import { createAuthMiddleware } from "better-auth/api";
+import { AIService } from "@/ai/ai.service";
+
+const ai = new AIService();
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -12,7 +16,7 @@ export const auth = betterAuth({
     enabled: true,
     minPasswordLength: 8,
     maxPasswordLength: 128,
-    autoSignIn: true,
+    // autoSignIn: true,
     sendResetPassword: async ({ user, url }) => {
       await sendForgotPasswordMail({
         to: user.email,
@@ -27,15 +31,19 @@ export const auth = betterAuth({
   },
   emailVerification: {
     sendVerificationEmail: async ({ user, url }) => {
+      const urlObj = new URL(url);
+      urlObj.searchParams.set("callbackURL", "/sign-in");
+      const newUrl = urlObj.toString();
+
       await sendVerificationEmailAction({
         to: user.email,
         subject: "Verify your email address",
         username: user.name,
-        verificationUrl: url,
+        verificationUrl: newUrl,
       });
     },
     sendOnSignUp: true,
-    autoSignInAfterVerification: true,
+    // autoSignInAfterVerification: true,
     expiresIn: 3600,
   },
   socialProviders: {
@@ -43,6 +51,17 @@ export const auth = betterAuth({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
+  },
+  hooks: {
+    after: createAuthMiddleware(async (ctx) => {
+      if (ctx.path.startsWith("/sign-in")) {
+        const newSession = ctx.context.newSession;
+        if (newSession) {
+          ai.addUser({ uid: newSession.user.id });
+        }
+      }
+      // Todo: add ai service for social signin also
+    }),
   },
   plugins: [nextCookies()],
 });
