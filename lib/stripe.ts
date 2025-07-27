9e5360a8751ecaa27e4f705725db68161fac1566
Harshalvk <harshalvkhobragade@gmail.com>
2025-07-27 22:40:53 +0530

    fix: remove unwanted functions

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -6,34 +6,6 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
   apiVersion: "2025-06-30.basil",
 });
 
-export const createCheckoutSession = async ({
-  userEmail,
-  userId,
-  priceId,
-}: {
-  userEmail: string;
-  userId: string;
-  priceId: string;
-}) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price: priceId,
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}&success=true`,
-    cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/user/billing`,
-    customer_email: userEmail,
-    metadata: {
-      userId,
-    },
-  });
-
-  return session;
-};
-
 export const handleCheckoutSession = async (event: Stripe.Checkout.Session) => {
   if (!event.metadata) {
     throw new Error("Missing metadata");
