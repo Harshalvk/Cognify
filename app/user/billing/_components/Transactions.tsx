03922d1b3ca19641906eb7295d7c95bd098f2aba
Harshalvk <harshalvkhobragade@gmail.com>
2025-07-28 21:31:16 +0530

    feat(ui): add transactions card for user billings

---
+++ app/user/billing/_components/Transactions.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { ArrowRightLeft } from "lucide-react";
+import React from "react";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+
+interface ITransactionsProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+const Transactions = <TData, TValue>({
+  data,
+  columns,
+}: ITransactionsProps<TData, TValue>) => {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Purchase Credits</CardTitle>
+        <CardDescription>Recent Transactions</CardDescription>
+        <CardAction>
+          <ArrowRightLeft />
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+        <div className="w-full flex justify-end gap-1 mt-2">
+          <Button
+            variant={"outline"}
+            size={"sm"}
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant={"outline"}
+            size={"sm"}
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Transactions;
--- app/user/billing/page.tsx
+++ app/user/billing/page.tsx
@@ -8,6 +8,9 @@ import { useSearchParams } from "next/navigation";
 import PaymentSuccessModal from "./_components/PaymentSuccessModal";
 import { trpcClient } from "@/trpc/clients/client";
 import Transactions from "./_components/Transactions";
+import { ColumnDef } from "@tanstack/react-table";
+import { format } from "date-fns";
+import { RouterOutputs } from "@/trpc/clients/types";
 
 const UserBillingPage = () => {
   const searchParams = useSearchParams();
@@ -16,12 +19,48 @@ const UserBillingPage = () => {
   const { data, isLoading } =
     trpcClient.creditBalance.myCreditBalance.useQuery();
 
+  const { data: transactions } =
+    trpcClient.transactions.myTransactions.useQuery();
+
+  const columns: ColumnDef<
+    RouterOutputs["transactions"]["myTransactions"][0]
+  >[] = [
+    {
+      accessorKey: "id",
+      header: "ID",
+    },
+    {
+      accessorKey: "notes",
+      header: "Detail",
+    },
+    {
+      accessorKey: "inputTokens",
+      header: "Input Tokens",
+    },
+    {
+      accessorKey: "outputTokens",
+      header: "Output Tokens",
+    },
+    {
+      accessorKey: "createdAt",
+      header: "Date",
+      cell: ({ row }) => {
+        const date = row.original.createdAt;
+        return <p>{format(date, "EEEE, MMMM do yyyy")}</p>;
+      },
+    },
+  ];
+
+  if (!transactions) {
+    return [];
+  }
+
   return (
     <section className="p-4 space-y-8 max-w-7xl mx-auto">
       <Title>Billing</Title>
       <UserCreditsCard userCredits={data?.balance || 0} isLoading={isLoading} />
       <PerchasePlan />
-      <Transactions />
+      <Transactions data={transactions} columns={columns} />
       {success && <PaymentSuccessModal />}
     </section>
   );
