6fc8307ddf9a36c1e8f2c1ba8ae083e6551b8680
Harshalvk <harshalvkhobragade@gmail.com>
2025-08-02 23:41:23 +0530

    feat: add edgestore provider to AppProvider

--- components/Providers/AppProvider.tsx
+++ components/Providers/AppProvider.tsx
@@ -2,18 +2,21 @@ import React from "react";
 import { ThemeProvider } from "./ThemeProvider";
 import { Toaster } from "../ui/sonner";
 import { TRPCReactProvider } from "@/trpc/clients/client";
+import { EdgeStoreProvider } from "@/lib/edgestore";
 
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ThemeProvider
-      attribute={"class"}
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <TRPCReactProvider>{children}</TRPCReactProvider>
-      <Toaster />
-    </ThemeProvider>
+    <EdgeStoreProvider>
+      <ThemeProvider
+        attribute={"class"}
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <Toaster />
+      </ThemeProvider>
+    </EdgeStoreProvider>
   );
 };
 
